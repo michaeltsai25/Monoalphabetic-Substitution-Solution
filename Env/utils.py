b62a548f9f31c70242ac7a4bed5789edf6f8f68c
michaeltsai25 <michaelktsai@gmail.com>
2023-02-07 22:30:05 -0500

    Created calcFreq

--- Env/utils.py
+++ Env/utils.py
@@ -5,3 +5,14 @@ def kl_divergence(cipherDistr, plainDistr):
     for i in range(26):
         div += cipherDistr[i]*log(cipherDistr[i]/plainDistr[i])
     return div
+
+def calcFreq(ciphText):
+    ciphText = ciphText.replace(" ", "")
+    text = "".join(set(ciphText))
+    freq = {}
+    for char in text: 
+        freq[char] = ciphText.count(char)/len(ciphText)
+    return sorted(freq.items(), key=lambda x: x[1])
+
+if __name__ == "__main__":
+    print(calcFreq("this is a test"))
