0d723f3169c8732e109c09ca93a24e475bad5c39
michaeltsai25 <michaelktsai@gmail.com>
2023-02-15 00:28:42 -0500

    Update runner.py

--- Env/runner.py
+++ Env/runner.py
@@ -1,4 +1,21 @@
 import utils
 
-class Runner:
-    pass
+class Guess:
+    def __init__(self, ciphertext):
+        self.ciph_text = ciphertext.replace(" ", "")
+        self.guess = ""
+        self.ct_to_pt = {}
+        self.reset()
+
+    def reset(self):
+        """Initializes the guess based on frequencies, modifies ct_to_pt as necessary"""
+        freq_list = "etaoinshrdlcumwfgypbvkjxqz"
+        desc_order = utils.calcFreq(self.ciph_text)
+        chars = []
+        for key, item in desc_order:
+            chars.append(key)
+        guess_desc_order = [freq_list[i] for i in range(len(desc_order))]
+        for i in range(len(chars)):
+            self.ct_to_pt[chars[i]] = guess_desc_order[i]
+        for char in self.ciph_text:
+            self.guess += self.ct_to_pt[char]
