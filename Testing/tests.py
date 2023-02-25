a1a2ebd41cdf0368e84fde0da0f2930fa7e5cd83
michaeltsai25 <michaelktsai@gmail.com>
2023-02-24 19:58:07 -0500

    Wrote some test cases, will fix errors later

--- Testing/tests.py
+++ Testing/tests.py
@@ -4,16 +4,27 @@ from guess import Guess
 import unittest
 
 class TestGuess(unittest.TestCase):
+
     def test_guess_reset(self):
-        pass
+        guess = Guess("abbcccddddeeeee")
+        self.assertEqual(guess.guess, "edcba")
+        self.assertEqual(guess.ct_to_pt, {'e':'e', 'd':'t', 'c':'a', 'b':'o', 'a':'i'})
 
-    def test_generate_data(self):
-        pass
+    def test_decrypted(self):
+        g = Guess("thisisatest")
+        self.assertEqual(g.decrypted(), True)
+        g = Guess("asdfghjkshouldbefalse")
+        self.assertEqual(g.decrypted(), False)
 
-    def time_num_words(self):
-        g = Guess("thisisatesttoseetheruntimeofthefunctionnumwords")
+    def test_num_words(self):
+        g = Guess("thisgdhfyeiseuyatest")
         num = g.num_words()
-        self.assertEqual(num[0], 14)
+        num = num[0]
+        self.assertEqual(num, 4)
+
+    def test_swap_general(self):
+        g = Guess("test")
+        self.assertEqual(g.swap_general(1), "tset")
 
 if __name__ == "__main__":
     unittest.main()
